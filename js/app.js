65ca5d836f5abb75c57f06179d85b87a48201266
LautyMel <lautymelgarejo46@gmail.com>
2026-08-25 12:12:47 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -276,7 +276,7 @@ function generatePDF() {
     const cliente = DOM.cliente.value || 'Cliente';
     const opt = {
         margin: 0,
-        filename: `Presupuesto_Sacchi_${cliente.replace(/\s+/g, '_')}.pdf`,
+        filename: `Presupuesto para ${cliente.replace(/\s+/g, '_')}.pdf`,
         image: { type: 'jpeg', quality: 0.98 },
         html2canvas: { scale: 2 },
         jsPDF: { unit: 'in', format: 'a4', orientation: 'portrait' }
